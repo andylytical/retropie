7ea8ed42874c08a57fcdf6a36867c100789d9fad
Andy Loftus <aloftus@gmail.com>
2023-07-31 00:41:27 +0000

    various fixes; tested on rpi lite

--- setup.sh
+++ setup.sh
@@ -2,12 +2,7 @@
 
 set -x
 
-BASE=$( dirname $0 )
-RC=bashrc
-RCDIR=~/.bashrc.d
-PROFILE=bash_profile
 GREP_PATTERN='CUSTOM INCLUDE FROM GITHUB/ANDYLYTICAL/RETROPIE'
-SUFFIX=$(date +%s)
 
 
 die() {
@@ -38,9 +33,11 @@ mount_roms_dir() {
   _pwd="password=${CIFSPASSWD}"
   _opts='nounix,noserverino,defaults,users,auto'
 
-  grep -q "$_tgt" /etc/fstab \
-  && >>/etc/fstab echo "$_src $_tgt cifs ${_usr},${_pwd},${_opts} 0 0"
-
+  grep -q "$_tgt" /etc/fstab || {
+    cat << EOF >>/etc/fstab
+$_src $_tgt cifs ${_usr},${_pwd},${_opts} 0 0
+EOF
+  }
   mount "$_tgt"
 }
 
@@ -49,19 +46,13 @@ disable_wifi() {
   local _tgt='/boot/config.txt'
   grep -q "$GREP_PATTERN" $_tgt || {
     cat << EOF >>$_tgt
+# $GREP_PATTERN
 dtoverlay=disable-wifi
 dtoverlay=disable-bt
 EOF
   }
   systemctl disable hciuart
 }
-
-
-# install_retroflag() {
-#   export QS_REPO=https://gitlab.com/andylytical/retroflag-picase
-#   curl https://raw.githubusercontent.com/andylytical/quickstart/master/quickstart.sh \
-#   | bash
-# }
 
 
 ### DO WORK
@@ -73,5 +64,3 @@ assert_var_is_not_empty CIFSPASSWD
 mount_roms_dir
 
 disable_wifi
-
-# install_retroflag
